a2042d020015bd95aa6360bf4f4d700868e46a21
abdullah.shahmeer <abdullah.shahmeer@laksol.com>
2026-01-08 22:39:39 +0500

    checking working

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,8 @@
 export const CONFIG = {
-    API_URL: 'https://unflighty-doretha-nondevotional.ngrok-free.dev', // Ngrok Tunnel URL
+    // API_URL: 'https://unflighty-doretha-nondevotional.ngrok-free.dev', // Ngrok Tunnel URL
+    API_URL: 'http://192.168.1.103:3000',
     ENABLE_BIOMETRIC: false, // User requested toggle
 };
 
 // http://192.168.0.148:3000
+// 1.103
